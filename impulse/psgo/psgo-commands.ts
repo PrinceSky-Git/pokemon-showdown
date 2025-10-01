2f75e1abe9e8e12d4503ebdc101b90c635821dd7
PrinceSky-Git <princeskygithub@gmail.com>
2025-10-01 09:51:39 +0530

    Update psgo-commands.ts

--- impulse/psgo/psgo-commands.ts
+++ impulse/psgo/psgo-commands.ts
@@ -21,7 +21,7 @@ function formatUserName(userId: string, bold: boolean = true, showGroup: boolean
 	return Impulse.nameColor(userId, bold, showGroup);
 }
 
-export const commands: Chat.Commands = {
+export const commands: Chat.ChatCommands = {
 	psgo: {
 		// Show card details
 		async show(target, room, user) {
